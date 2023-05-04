33cd5e41a059171399d74c423084a561e29e85cd
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-04 07:57:39 -0700

    Update 6-bonus.sql to fix bugs

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,14 +3,14 @@
 DELIMITER |
 
 CREATE PROCEDURE AddBonus (
-  IN user_id INTEGER,
+  IN user_id INT,
   IN project_name VARCHAR(255),
-  IN score FLOAT
+  IN score float
 )
 BEGIN
   INSERT INTO projects (name)
-  SELECT procedure_name FROM DUAL
-  WHERE NOT EXISTS (SELECT * FROM projects WHERE name = procedure_name);
+  SELECT project_name FROM DUAL
+  WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
   INSERT INTO corrections (user_id, project_id, score)
-  VALUES (user_id, (SELECT id FROM projects WHERE name = procedure_name), score);
+  VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
