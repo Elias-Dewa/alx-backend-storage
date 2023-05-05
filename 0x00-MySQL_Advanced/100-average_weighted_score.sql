e539cdfab22f8cb8c08e5e1fde0ebbca173bd800
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-05 01:14:07 -0700

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,10 +4,11 @@ DELIMITER |
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
+  UPDATE users
   DECLARE result FLOAT;
   SET result = (
     SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) FROM corrections
     JOIN projects ON projects.id = corrections.project_id
     WHERE corrections.user_id = user_id)
-  UPDATE users SET average_score = result WHERE users.id = user_id;
+  WHERE users.id = user_id;
 END;
