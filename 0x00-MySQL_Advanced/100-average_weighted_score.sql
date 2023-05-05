b91dcd6b56c22d5b80de601d32bcd72647c26a93
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-05 01:29:54 -0700

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,6 +7,6 @@ BEGIN
   UPDATE users SET average_score = (
     SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) FROM corrections
     JOIN projects ON projects.id = corrections.project_id
-    WHERE corrections.user_id = users.id);
+    WHERE corrections.user_id = user_id)
   WHERE users.id = user_id;
 END;
