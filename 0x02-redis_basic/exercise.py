81baebb51b33ddfb12b2e5d8b1c4e163c0beb05b
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-10 04:31:08 -0700

    Redis: task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 import redis
 
@@ -18,14 +18,28 @@ class Cache():
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """method that generate a random key, store the input data in Redis
-        using the random key
-
-        Args:
-            data (Union[str, bytes, int, float]): a data argument
-
-        Returns:
-            str: return the key
+        using the random key and return the key
         """
         generate_random_key = str(uuid4())
         self._redis.set(generate_random_key, data)
         return generate_random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """method that take a key string argument and
+        an optional Callable argument
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key):
+        """method to automatically parametrize Cache.get
+        """
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key):
+        """method to automatically parametrize Cache.get
+        """
+        return self._redis.get(key, int)
